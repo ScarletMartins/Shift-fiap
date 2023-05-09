54c7552c1fe849d5ef796b30d708a99ca83d85f9
ScarletMartins <scarletgam@icloud.com>
2023-05-09 16:09:27 -0400

    Criacao de entidades do projeto

---
+++ src/main/java/br/com/fiap/rentacar/entity/Modelo.java
@@ -0,0 +1,14 @@
+package br.com.fiap.rentacar.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Modelo {
+
+    private Long id;
+    private String nome;
+    private Double potencia;
+    private Marca marca;
+}
