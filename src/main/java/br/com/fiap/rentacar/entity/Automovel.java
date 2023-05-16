9ff5f35713314aa16903ce3b3457a94cfb9ef0cb
ScarletMartins <scarletgam@icloud.com>
2023-05-16 18:59:42 -0400

    Mapeamento de entidades com JPA

--- src/main/java/br/com/fiap/rentacar/entity/Automovel.java
+++ src/main/java/br/com/fiap/rentacar/entity/Automovel.java
@@ -1,6 +1,7 @@
 package br.com.fiap.rentacar.entity;
 
 import br.com.fiap.rentacar.enums.TipoCombustivelEnum;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -8,12 +9,28 @@ import java.util.List;
 
 @Getter
 @Setter
+@Entity
+@Table(name = "TBL_AUTOMOVEL")
 public class Automovel {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "ID_AUTOMOVEL")
     private Long id;
+
+    @Column(name = "NR_ANO_FABRICACAO")
     private Integer anoFabricacao;
+
+    @Column(name = "NR_ANO_MODELO")
     private Integer anoModelo;
+
+    @Column(name = "TP_COMBUSTIVEL")
+    @Enumerated(EnumType.STRING) //indica que é uma Enum e que deve ser armazenado o texto
     private TipoCombustivelEnum tipoCombustivel;
+
+    @Transient
     private Modelo modelo;
+
+    @Transient
     private List<Acessorio> acessorios;
 }
--- src/main/java/br/com/fiap/rentacar/entity/Modelo.java
+++ src/main/java/br/com/fiap/rentacar/entity/Modelo.java
@@ -1,14 +1,28 @@
 package br.com.fiap.rentacar.entity;
 
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.math.BigDecimal;
+
 @Getter
 @Setter
+@Entity
+@Table(name = "TBL_MODELO")
 public class Modelo {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "ID_MODELO")
     private Long id;
+
+    @Column(name = "TX_NOME")
     private String nome;
-    private Double potencia;
+
+    @Column(name = "NR_POTENCIA")
+    private BigDecimal potencia;
+
+    @Transient //marca um atributo como nao persistente / não mapea o atributo como coluna no db
     private Marca marca;
 }
