824591cf7a8438fd7dd24cf3fdbff799dc2b621e
ScarletMartins <scarletgam@icloud.com>
2023-05-17 18:39:42 -0400

    Crianção de repositorios Spring

---
+++ src/main/java/br/com/fiap/rentacar/repository/AcessorioRepository.java
@@ -0,0 +1,8 @@
+package br.com.fiap.rentacar.repository;
+
+import br.com.fiap.rentacar.entity.Acessorio;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AcessorioRepository extends JpaRepository<Acessorio, Long> {
+    
+}
