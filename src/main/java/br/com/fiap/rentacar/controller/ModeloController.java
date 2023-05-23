37ba03ffab091c40d8bef38e29b75c8d7d9dae05
ScarletMartins <scarletgam@icloud.com>
2023-05-23 13:58:14 -0400

    Criação das camadas de controle

--- src/main/java/br/com/fiap/rentacar/controller/ModeloController.java
+++ src/main/java/br/com/fiap/rentacar/controller/ModeloController.java
@@ -1,9 +1,13 @@
 package br.com.fiap.rentacar.controller;
 
+import br.com.fiap.rentacar.entity.Modelo;
 import br.com.fiap.rentacar.repository.ModeloRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/modelos")
@@ -11,4 +15,45 @@ public class ModeloController {
 
     @Autowired
     private ModeloRepository modeloRepository;
+
+    @GetMapping
+    public List<Modelo> findAll() {
+        return modeloRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Modelo> findById(@PathVariable Long id) {
+        Optional<Modelo> result = modeloRepository.findById(id);
+        if (result.isEmpty()) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(result.get());
+    }
+
+    @PostMapping
+    public ResponseEntity<Modelo> insert(@RequestBody Modelo modelo) {
+
+        Modelo saved = modeloRepository.save(modelo);
+        return ResponseEntity.ok(saved);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Modelo> update(@PathVariable Long id, @RequestBody Modelo modelo) {
+        if (!modeloRepository.existsById(id)) {
+            return ResponseEntity.notFound().build();
+        }
+        Modelo updated = modeloRepository.save(modelo);
+        return ResponseEntity.ok(updated);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Modelo> delete(@PathVariable Long id) {
+        Optional<Modelo> result = modeloRepository.findById(id);
+        if (result.isEmpty()) {
+            return ResponseEntity.notFound().build();
+        }
+        modeloRepository.deleteById(id);
+        return ResponseEntity.ok().build();
+    }
+
 }
